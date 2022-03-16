837a32c988839b0b3301b2ce34dce8a1ebcaa4e3
Mateusz.Majka <mateusz.majka.zsl@gmail.com>
2022-03-16 17:04:39 +0100

    lab -3

---
+++ lab-3/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace lab_3
+{
+    public abstract class Product
+    {
+        public virtual decimal Price { get; init; }
+        public abstract decimal GetVatPrice();
+    }
+
+
+
+
+    class Computer: Product
+    {
+        public decimal Vat { get; init; }
+
+        public override decimal GetVatPrice()
+        {
+            return Price * Vat / 100m;
+        }
+    }
+
+    class Paint: Product
+    {
+        public decimal Vat { get; init; }
+
+        public decimal Capacity { get; init; }
+        public decimal PriceUnit { get; init; }
+
+        public override decimal GetVatPrice()
+        {
+            return PriceUnit * Capacity * Vat / 100m;
+        }
+
+        public override decimal Price
+        {
+            get
+            {
+                return PriceUnit * Capacity;
+            }
+        }
+    }
+
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product[] shop = new Product[4];
+            shop[0] = new Computer() { Price = 2000, Vat = 23};
+            shop[1] = new Paint() { PriceUnit = 15, Capacity=5, Vat = 15 };
+            shop[2] = new Computer() { Price = 1780, Vat = 23 };
+            shop[3] = new Paint() { PriceUnit = 18, Capacity = 8, Vat = 15 };
+        }
+    }
+}
