952eb70af2c345cb8a39fc24f31fa984484b01b8
Mateusz.Majka <mateusz.majka.zsl@gmail.com>
2022-05-04 17:14:02 +0200

    lab-8 part1

---
+++ lab-8/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace lab_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public record Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int AuthorId { get; set; }
+    }
+
+    public record Author
+        //all properties need get and set
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    class AppContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("DATASOURCE=d:\\database\\data.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
